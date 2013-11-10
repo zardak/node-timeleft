63342407b5e6328deaabd292efbeb248526916c3
zardak <zardakss@gmail.com>
2013-11-10 18:27:36 +0200

    Updated for current nodejs version

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 module.exports = function(timer) {
-    return Math.ceil(timer._idleStart.getTime() + timer._idleTimeout - Date.now());
+    return Math.ceil(timer._idleStart + timer._idleTimeout - Date.now());
 };
